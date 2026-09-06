dca908a5b68405f3a0549e2d2b327954633b1797
noise23 <noise23@users.noreply.github.com>
2026-09-06 19:19:00 +0300

    Delete boost/filesystem/convenience.hpp

--- src/init.cpp
+++ src/init.cpp
@@ -15,7 +15,6 @@
 #include "key.h"
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
-#include <boost/filesystem/convenience.hpp>
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <openssl/crypto.h>
